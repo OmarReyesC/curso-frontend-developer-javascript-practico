96a9865df020012933a8910a7c0dec44f75c1702
OmarReyesC <omareyescar@gmail.com>
2024-07-05 17:25:27 -0600

    After second class, menu is deployed on mobile navbar.

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+
 
 function toggleDesktopMenu() {
    desktopMenu.classList.toggle('inactive');
 }
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
+}
+
+//Experiment. Not to be kept.
+/*menuEmail.addEventListener('click', toggleMenu(menuEmail));
+menuHamIcon.addEventListener('click', toggleMobileMenu(menuHamIcon));
+
+function toggleMenu(menuInstance) {
+    menuInstance.classList.toggle('inactive');
+};*/
